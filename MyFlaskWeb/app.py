601598d18353743c66148fdc433b473504c23783
soonang <nr97819@naver.com>
2021-11-05 09:38:13 +0900

    수정

--- MyFlaskWeb/app.py
+++ MyFlaskWeb/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from re import U
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
 @app.route('/') #접속할 URL
 def main():
-	return render_template('index.html') #예제 템플릿
+	return render_template('index.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+	if request.method =='GET':
+		return render_templ	ate('register.html')
+		
+	elif request.method =='POST':
+		# id=request.form.get('inputId')
+		# pw=request.form.get('inputId')
+		
+		inputId=request.args.get('inputId')
+		inputPw=request.args.get('inputPw')
+		str = '%s' % inputId
+		str += '%s' % inputPw
+		return str
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
