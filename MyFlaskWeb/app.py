1743d0b07380ec6e34e96048d4ac2e8228066df7
soonang <nr97819@naver.com>
2021-11-09 13:50:13 +0900

    html 파일 flask 파일 수정

--- MyFlaskWeb/app.py
+++ MyFlaskWeb/app.py
@@ -24,5 +24,29 @@ def register():
 		
 		return render_template('index.html', str=str)
 
+@app.route('/agreement', methods=['GET','POST']) # 개인 정보 동의 페이지
+def agree():
+	if request.method =='GET':
+		return render_template('agreement.html')
+		
+	elif request.method =='POST':
+		return render_template('index.html')
+
+@app.route('/uploading', methods=['GET','POST']) # 진단서 업로드 페이지
+def upload():
+	if request.method =='GET':
+		return render_template('uploading.html')
+		
+	elif request.method =='POST':
+		return render_template('index.html')
+
+@app.route('/products', methods=['GET','POST']) # 상품 목록 페이지
+def product():
+	if request.method =='GET':
+		return render_template('products.html')
+		
+	elif request.method =='POST':
+		return render_template('index.html')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
